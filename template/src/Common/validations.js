75a9f2addce669a5d868dd1e7cfb58d08a3a225d
Timson <a_sa_f@126.com>
2017-08-01 23:19:20 +0800

    add the demo validation

--- template/src/Common/validations.js
+++ template/src/Common/validations.js
@@ -1,8 +1,14 @@
-function number(value = '') {
-	if (/^\d+(\.\d+)?$/ig.test(value)) return true
+function number(value = '', max, min = 1) {
+	if (new RegExp(`^\\d{${min},${max}}$`)) return true
+	return false
+}
+
+function phone(phone) {
+	if (/^[1][358][0-9]{9}$/ig.test(value)) return true
 	return false
 }
 
 export default {
-	number
+	number,
+	phone,
 }
