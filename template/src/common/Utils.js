fb6d517b5be69ce3507a7477e73f5aefc5db7a78
Timson020 <a_sa_f@126.com>
2018-11-19 11:09:17 +0800

    modify the webpack.config.js && update the package

--- template/src/common/Utils.js
+++ template/src/common/Utils.js
@@ -1,21 +1,24 @@
-async function ajax(url, body = {}, method = 'post') {
-	let _res
-	try {
-		_res = await this.$http({
-			method: method,
-			url: url,
-			body: body,
-			emulateJSON: true
+import axios from 'axios'
+
+async function fetch({ url, body = {}, method = 'post' }) {
+	return new Promise((resolve) => {
+		axios({
+			url,
+			method,
+			headers: { 'content-type': 'application/json; ' },
+			data: body,
+			timeout: 15000,
+		}).then((res) => {
+			resolve({ ...res })
+		}).catch((err) => {
+			resolve({ code: 500, data: {}, msg: err.toString() })
 		})
-	} catch (err) {
-		return { code: 400, data: null, msg: err }
-	}
-	return { code: 200, data: _res, msg: '' }
+	})
 }
 
 const utils = {
 	// 请求
-	ajax,
+	fetch,
 }
 
 export default utils
--- template/src/main.js
+++ template/src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Router from 'vue-router'
-import Resource from 'vue-resource'
 
 import { vuex, routers } from './config'
+import { Utils } from './common'
 
 import './scss/global'
 
 Vue.use(Vuex)
 Vue.use(Router)
-Vue.use(Resource)
+Vue.prototype.fetch = Utils.fetch
 
 // 实例化
 const router = new Router(routers),
